71cc5258c7c2c4e4914181595cb2291296d4f4ab
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-09-02 23:41:33 +0200

    Refactor

--- ProjectZen/Program.cs
+++ ProjectZen/Program.cs
@@ -1,42 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Xml;
 
 namespace ProjectZen
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var xmlDoc = new XmlDocument();
+            var firstPandenFile = "c:/src/projects/project-zen/tmp/small-zips-unpacked/9999PND08082020-000001.xml";
+            var pandenNodes = GetPandenElements(firstPandenFile);
 
-            xmlDoc.Load("c:/src/projects/project-zen/tmp/small-zips-unpacked/9999PND08082020-000001.xml");
+            //var count = xmlDocument.DocumentElement.ChildNodes.Count;
 
-            var nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
-            nsmgr.AddNamespace("xb", "http://www.kadaster.nl/schemas/bag-verstrekkingen/extract-deelbestand-lvc/v20090901");
-            nsmgr.AddNamespace("product_LVC", "http://www.kadaster.nl/schemas/bag-verstrekkingen/extract-producten-lvc/v20090901");
-            nsmgr.AddNamespace("bag_LVC", "http://www.kadaster.nl/schemas/imbag/lvc/v20090901");
+            //Console.WriteLine($"Count: {count}");
+            //Console.WriteLine($"Name: {xmlDocument.DocumentElement.Name}");
 
-            var count = xmlDoc.DocumentElement.ChildNodes.Count;
+            //Console.WriteLine($"Name: {xmlDocument.DocumentElement.FirstChild.Name}");
 
-            Console.WriteLine($"Count: {count}");
-            Console.WriteLine($"Name: {xmlDoc.DocumentElement.Name}");
-
-            Console.WriteLine($"Name: {xmlDoc.DocumentElement.FirstChild.Name}");
-
-            var nodes = xmlDoc.SelectNodes("/xb:BAG-Extract-Deelbestand-LVC/xb:antwoord/xb:producten/product_LVC:LVC-product/bag_LVC:Pand", nsmgr);
-            Console.WriteLine(nodes.Count);
+            //Console.WriteLine(nodes.Count);
 
             var panden = new List<Pand>();
 
-            foreach (XmlNode pandXml in nodes)
+            foreach (XmlNode pandXml in pandenNodes)
             {
-                panden.Add(new Pand
-                {
-                    Id = pandXml["bag_LVC:identificatie"].InnerText,
-                    ConstructionYear = int.Parse(pandXml["bag_LVC:bouwjaar"].InnerText)
-                });
+                panden.Add(Pand.From(pandXml));
             }
+        }
+
+        private static XmlNodeList GetPandenElements(string filename)
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(filename);
+
+            var namespaceManager = new XmlNamespaceManager(xmlDocument.NameTable);
+            namespaceManager.AddNamespace("xb", "http://www.kadaster.nl/schemas/bag-verstrekkingen/extract-deelbestand-lvc/v20090901");
+            namespaceManager.AddNamespace("product_LVC", "http://www.kadaster.nl/schemas/bag-verstrekkingen/extract-producten-lvc/v20090901");
+            namespaceManager.AddNamespace("bag_LVC", "http://www.kadaster.nl/schemas/imbag/lvc/v20090901");
+
+            var nodes = xmlDocument.SelectNodes("/xb:BAG-Extract-Deelbestand-LVC/xb:antwoord/xb:producten/product_LVC:LVC-product/bag_LVC:Pand", namespaceManager);
+
+            return nodes;
         }
     }
 
@@ -48,6 +51,15 @@ namespace ProjectZen
         public override string ToString()
         {
             return $"ID: {Id}, ConstructionYear: {ConstructionYear}";
+        }
+
+        public static Pand From(XmlNode xmlNode)
+        {
+            return new Pand
+            {
+                Id = xmlNode["bag_LVC:identificatie"].InnerText,
+                ConstructionYear = int.Parse(xmlNode["bag_LVC:bouwjaar"].InnerText)
+            };
         }
     }
 }
