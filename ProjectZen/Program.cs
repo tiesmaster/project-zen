615633866f4baa5bf6a565594f3a4e6c972d8344
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-09-02 23:38:00 +0200

    Read the first PND xml, and parse to ID, and construction year

--- ProjectZen/Program.cs
+++ ProjectZen/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Xml;
 
 namespace ProjectZen
 {
@@ -6,7 +8,46 @@ namespace ProjectZen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var xmlDoc = new XmlDocument();
+
+            xmlDoc.Load("c:/src/projects/project-zen/tmp/small-zips-unpacked/9999PND08082020-000001.xml");
+
+            var nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("xb", "http://www.kadaster.nl/schemas/bag-verstrekkingen/extract-deelbestand-lvc/v20090901");
+            nsmgr.AddNamespace("product_LVC", "http://www.kadaster.nl/schemas/bag-verstrekkingen/extract-producten-lvc/v20090901");
+            nsmgr.AddNamespace("bag_LVC", "http://www.kadaster.nl/schemas/imbag/lvc/v20090901");
+
+            var count = xmlDoc.DocumentElement.ChildNodes.Count;
+
+            Console.WriteLine($"Count: {count}");
+            Console.WriteLine($"Name: {xmlDoc.DocumentElement.Name}");
+
+            Console.WriteLine($"Name: {xmlDoc.DocumentElement.FirstChild.Name}");
+
+            var nodes = xmlDoc.SelectNodes("/xb:BAG-Extract-Deelbestand-LVC/xb:antwoord/xb:producten/product_LVC:LVC-product/bag_LVC:Pand", nsmgr);
+            Console.WriteLine(nodes.Count);
+
+            var panden = new List<Pand>();
+
+            foreach (XmlNode pandXml in nodes)
+            {
+                panden.Add(new Pand
+                {
+                    Id = pandXml["bag_LVC:identificatie"].InnerText,
+                    ConstructionYear = int.Parse(pandXml["bag_LVC:bouwjaar"].InnerText)
+                });
+            }
+        }
+    }
+
+    public class Pand
+    {
+        public string Id { get; set; }
+        public int ConstructionYear { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id}, ConstructionYear: {ConstructionYear}";
         }
     }
 }
