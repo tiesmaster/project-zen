7d9a4d988be687f0c8122a74ab2fb735d0002bce
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-09-08 00:16:22 +0200

    Add Bag Pand type

---
+++ ProjectZen.Domain/Bag/Pand.cs
@@ -0,0 +1,14 @@
+﻿namespace Tiesmaster.ProjectZen.Domain.Bag
+{
+    public class Pand
+    {
+        public Pand(string id, int constructionYear)
+        {
+            Id = id;
+            ConstructionYear = constructionYear;
+        }
+
+        public string Id { get; }
+        public int ConstructionYear { get; }
+    }
+}
