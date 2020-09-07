2d3cd95fda131bb089b9293a04f0ee2d52ae6871
Thijs Brobbel <t.brobbel@buddypayment.nl>
2020-09-08 00:12:43 +0200

    Add Building type

---
+++ ProjectZen.Domain/Building.cs
@@ -0,0 +1,14 @@
+﻿namespace Tiesmaster.ProjectZen.Domain
+{
+    public class Building
+    {
+        public Building(string id, int constructionYear)
+        {
+            Id = id;
+            ConstructionYear = constructionYear;
+        }
+
+        public string Id { get; }
+        public int ConstructionYear { get; }
+    }
+}
