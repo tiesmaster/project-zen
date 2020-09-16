8359888d6fce586dc74326433cbd9ac83ecdd7a6
Thijs Brobbel <thijsbrobbel@gmail.com>
2020-09-17 00:01:01 +0200

    Add new BagImporter unit test project

---
+++ ProjectZen.BagImporter.UnitTests/BagParserTests.cs
@@ -0,0 +1,15 @@
+using FluentAssertions;
+
+using Xunit;
+
+namespace Tiesmaster.ProjectZen.BagImporter.UnitTests
+{
+    public class BagParserTests
+    {
+        [Fact]
+        public void BreakIceTest()
+        {
+            true.Should().BeTrue();
+        }
+    }
+}
