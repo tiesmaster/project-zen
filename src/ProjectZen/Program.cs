7b1e1d561ab6baf245d0a5da0559395dec1ef99f
Thijs Brobbel <thijsbrobbel@gmail.com>
2020-09-25 23:46:17 +0200

    Persist WPLs to RavenDB

--- src/ProjectZen/Program.cs
+++ src/ProjectZen/Program.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 using MoreLinq;
 
@@ -10,7 +9,6 @@ using NodaTime;
 using Raven.Client.Documents;
 
 using Tiesmaster.ProjectZen.BagImporter;
-using Tiesmaster.ProjectZen.Domain;
 
 namespace Tiesmaster.ProjectZen
 {
@@ -24,32 +22,33 @@ namespace Tiesmaster.ProjectZen
                 "c:/src/projects/project-zen/tmp/small-zips-unpacked/",
                 maxFilesToProcess);
 
-            //var woonplaatsen = buildingImporter.ReadWoonplaatsen();
+            var woonplaatsen = buildingImporter.ReadWoonplaatsen();
+            PersistToRavenDB(woonplaatsen);
 
-            var panden = buildingImporter.ReadPanden();
+            //var panden = buildingImporter.ReadPanden();
             //var verblijfsobjecten = buildingImporter.ReadVerblijfsobjecten();
             //var nummeraanduidingen = buildingImporter.ReadNummeraanduidingen();
             //var openbareRuimten = buildingImporter.ReadOpenbareRuimten();
 
-            var buildings = panden.Select(x => new Building(x.Id, x.ConstructionYear));
+            //var buildings = panden.Select(x => new Building(x.Id, x.ConstructionYear));
 
-            PersistBuildings(buildings);
+            //PersistToRavenDB(buildings);
         }
 
-        private static void PersistBuildings(IEnumerable<Building> buildings)
+        private static void PersistToRavenDB(IEnumerable<object> objects)
         {
-            Console.WriteLine("Start persisting Buildings");
+            Console.WriteLine("Start persisting");
             var totalSw = Stopwatch.StartNew();
 
             using var store = OpenDocumentStore();
 
             var bulkInsert = store.BulkInsert();
-            foreach (var (buildingBatch, index) in buildings.Batch(10_000).WithIndex())
+            foreach (var (objectBatch, index) in objects.Batch(10_000).WithIndex())
             {
                 Console.WriteLine($"Saving batch {index}");
                 var batchSw = Stopwatch.StartNew();
 
-                foreach (var building in buildingBatch)
+                foreach (var building in objectBatch)
                 {
                     bulkInsert.Store(building);
                 }
@@ -57,7 +56,7 @@ namespace Tiesmaster.ProjectZen
                 Console.WriteLine($"Saved batch {index} of 10.000 (in {batchSw.Elapsed})");
             }
 
-            Console.WriteLine($"Finished persisting Buildings (in {totalSw.Elapsed})");
+            Console.WriteLine($"Finished persisting objects (in {totalSw.Elapsed})");
         }
 
         private static DocumentStore OpenDocumentStore()
