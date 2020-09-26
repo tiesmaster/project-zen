04ac9ed16b60d9c68f96e3dfae2c52b0c380b6ca
Thijs Brobbel <thijsbrobbel@gmail.com>
2020-09-26 19:58:02 +0200

    Create RavenDB database, if absent

--- src/ProjectZen/Program.cs
+++ src/ProjectZen/Program.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 using MoreLinq;
 
 using NodaTime;
 
 using Raven.Client.Documents;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
 
 using Tiesmaster.ProjectZen.BagImporter;
 
@@ -16,16 +19,16 @@ namespace Tiesmaster.ProjectZen
     {
         public static void Main()
         {
-            var maxFilesToProcess = 100;
+            var maxFilesToProcess = 1;
             var buildingImporter = new BuildingBagImporter(
                 SystemClock.Instance,
                 "c:/src/projects/project-zen/tmp/small-zips-unpacked/",
                 maxFilesToProcess);
 
-            buildingImporter.ReadNummeraanduidingen();
+            var nums = buildingImporter.ReadNummeraanduidingen();
 
             //var buildings = buildingImporter.ReadBuildings();
-            //PersistToRavenDB(buildings);
+            PersistToRavenDB(nums);
         }
 
         private static void PersistToRavenDB(IEnumerable<object> objects)
@@ -54,15 +57,32 @@ namespace Tiesmaster.ProjectZen
 
         private static DocumentStore OpenDocumentStore()
         {
+            const string databaseName = "ProjectZen";
             var store = new DocumentStore
             {
                 Urls = new[] { "http://localhost:8080" },
-                Database = "ProjectZen"
+                Database = databaseName
             };
 
             store.Initialize();
+            CreateDatabaseIfAbsent(store, databaseName);
 
             return store;
+        }
+
+        private static void CreateDatabaseIfAbsent(DocumentStore documentStore, string databaseName)
+        {
+            var existingDatabases = documentStore.Maintenance.Server.Send(new GetDatabaseNamesOperation(0, 128));
+            if (!existingDatabases.Any(name => name == databaseName))
+            {
+                //_logger.LogInformation("RavenDB database '{DatabaseName}' is absent. Creating database...", databaseName);
+                documentStore.Maintenance.Server.Send(new CreateDatabaseOperation(new DatabaseRecord(databaseName)));
+                //_logger.LogInformation("RavenDB database '{DatabaseName}' created.", databaseName);
+            }
+            else
+            {
+                //_logger.LogDebug("RavenDB database '{DatabaseName}' already exists. Skipping creation.", databaseName);
+            }
         }
     }
 }
