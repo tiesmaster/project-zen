92b1b4ed91c2d31c85ee33cd0872741409f84fff
Thijs Brobbel <thijsbrobbel@gmail.com>
2020-09-26 08:58:48 +0200

    Read all BAG objects into dicts

--- src/ProjectZen.Domain/Building.cs
+++ src/ProjectZen.Domain/Building.cs
@@ -1,14 +1,42 @@
-﻿namespace Tiesmaster.ProjectZen.Domain
+namespace Tiesmaster.ProjectZen.Domain
 {
     public class Building
     {
-        public Building(string id, int constructionYear)
+        public Building(string id, int constructionYear, Address address)
         {
             Id = id;
             ConstructionYear = constructionYear;
+            Address = address;
         }
 
         public string Id { get; }
         public int ConstructionYear { get; }
+        public Address Address { get; }
+    }
+
+    public class Address
+    {
+        public Address(
+            string street,
+            int houseNumber,
+            char? houseLetter,
+            string houseNumberAddition,
+            string postalCode,
+            string city)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            HouseLetter = houseLetter;
+            HouseNumberAddition = houseNumberAddition;
+            PostalCode = postalCode;
+            City = city;
+        }
+
+        public string Street { get; }
+        public int HouseNumber { get; }
+        public char? HouseLetter { get; }
+        public string HouseNumberAddition { get; }
+        public string PostalCode { get; }
+        public string City { get; }
     }
 }
--- src/ProjectZen/Program.cs
+++ src/ProjectZen/Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 using MoreLinq;
 
@@ -9,37 +11,85 @@ using NodaTime;
 using Raven.Client.Documents;
 
 using Tiesmaster.ProjectZen.BagImporter;
+using Tiesmaster.ProjectZen.Domain;
+using Tiesmaster.ProjectZen.Domain.Bag;
 
 namespace Tiesmaster.ProjectZen
 {
+    public class VboComparer : IEqualityComparer<(BagVerblijfsobject vbo, string pandId)>
+    {
+        public bool Equals([AllowNull] (BagVerblijfsobject vbo, string pandId) x, [AllowNull] (BagVerblijfsobject vbo, string pandId) y)
+        {
+            return x.pandId == y.pandId;
+        }
+
+        public int GetHashCode([DisallowNull] (BagVerblijfsobject vbo, string pandId) obj)
+        {
+            return obj.pandId.GetHashCode();
+        }
+    }
+
     public static class Program
     {
         public static void Main()
         {
-            var maxFilesToProcess = 100;
+            var maxFilesToProcess = 1000;
             var buildingImporter = new BuildingBagImporter(
                 SystemClock.Instance,
                 "c:/src/projects/project-zen/tmp/small-zips-unpacked/",
                 maxFilesToProcess);
 
-            var woonplaatsen = buildingImporter.ReadWoonplaatsen();
-            PersistToRavenDB(woonplaatsen);
+            var woonplaatsen = buildingImporter.ReadWoonplaatsen().ToDictionary(x => x.Id, x => x);
 
-            var openbareRuimten = buildingImporter.ReadOpenbareRuimten();
-            PersistToRavenDB(openbareRuimten);
+            var openbareRuimten = buildingImporter.ReadOpenbareRuimten().ToDictionary(x => x.Id, x => x);
 
-            var nummeraanduidingen = buildingImporter.ReadNummeraanduidingen();
-            PersistToRavenDB(nummeraanduidingen);
+            var nummeraanduidingen = buildingImporter.ReadNummeraanduidingen().ToDictionary(x => x.Id, x => x);
 
-            var verblijfsobjecten = buildingImporter.ReadVerblijfsobjecten();
-            PersistToRavenDB(verblijfsobjecten);
+            //var pandVboMapping = buildingImporter
+            //    .ReadVerblijfsobjecten()
+            //    .SelectMany(x => x.RelatedPanden, (vbo, pandId) => (vbo, pandId))
+            //    .GroupBy(x => x.pandId)
+            //    .ToDictionary(x => x.Key, x => x.Select(x => x.vbo).ToList());
+
+            var verblijfsobjecten = buildingImporter
+                .ReadVerblijfsobjecten()
+                .SelectMany(x => x.RelatedPanden, (vbo, pandId) => (vbo, pandId))
+                .Distinct(new VboComparer())
+                .ToDictionary(x => x.pandId, x => x.vbo);
 
             var panden = buildingImporter.ReadPanden();
-            PersistToRavenDB(panden);
+
+            var buildings = panden.Select(pand => ToBuilding(pand, verblijfsobjecten, nummeraanduidingen, openbareRuimten, woonplaatsen));
+
+            var rotterBuildings = buildings.Where(b => b.Address.City == "Rotterdam").ToList();
 
             //var buildings = panden.Select(x => new Building(x.Id, x.ConstructionYear));
 
-            //PersistToRavenDB(buildings);
+            PersistToRavenDB(buildings);
+        }
+
+        private static Building ToBuilding(
+            BagPand pand,
+            Dictionary<string, BagVerblijfsobject> verblijfsobjecten,
+            Dictionary<string, BagNummeraanduiding> nummeraanduidingen,
+            Dictionary<string, BagOpenbareRuimte> openbareRuimten,
+            Dictionary<string, BagWoonplaats> woonplaatsen)
+        {
+            var relatedVbo = verblijfsobjecten[pand.Id];
+            var relatedNum = nummeraanduidingen[relatedVbo.RelatedMainAddress];
+            var relatedOpr = openbareRuimten[relatedNum.RelatedOpenbareRuimte];
+            var relatedWpl = woonplaatsen[relatedOpr.RelatedWoonplaats];
+
+            return new Building(
+                pand.Id,
+                pand.ConstructionYear,
+                new Address(
+                    relatedOpr.Name,
+                    relatedNum.HouseNumber,         // TODO: Join these...
+                    relatedNum.HouseLetter,
+                    relatedNum.HouseNumberAddition,
+                    relatedNum.PostalCode,
+                    relatedWpl.Name));
         }
 
         private static void PersistToRavenDB(IEnumerable<object> objects)
